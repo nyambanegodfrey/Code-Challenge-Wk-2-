02f0148e9efbde146815a238bb8268e4e2febcba
Nyambane F Godfrey <nyambanego@gmail.com>
2024-07-09 14:46:33 +0300

    Add Final Changes

--- Development/code/phase-1/Code-Challenge-Wk-2-/Shopping.js
+++ Development/code/phase-1/Code-Challenge-Wk-2-/Shopping.js
@@ -1,15 +1,56 @@
-$function(){
-  let callback =function (event) {
-    event.preventDefault();
-    let input=$('input[type=text][name=item]')
+const shoppingList = JSON.parse(localStorage.getItem("items")) || [];
 
-    value=input.value(),
+// grab element references from the DOM
+const listContainer = document.getElementsByClassName("list-container")[0];
+const addItemInput = document.getElementById("item");
+const addItemButton = document.querySelector(".add-item-button");
 
-    need=($(event.target).attr('id')===addNeeded),
+// const GLOBAL_ITEM = "";
+// loop through the initial items
+const initialItems = (items) => {
+  listContainer.innerHTML = "";
+  items.forEach((item, i) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("shopping-list-item");
+    listItem.innerHTML = `<span class='shopping-list-item-${i}'>${item}</span>  <div class="purchased-container"><button onclick="markPurchased('shopping-list-item-${i}')" class="mark-purchased-button">mark purchased</button> <span onclick="removeParent('${item}')" class="times">&times;</span></div>`;
+    listContainer.appendChild(listItem);
+  });
 
-    item=$('<li><input type="checkbox" name="item">'+ value + '<a href="#"remove</a></li>'),
-    
-    list = (need)? $('ul').first(): $('ul').last();
-    
+  if (items.length > 0) {
+    const clearButton = document.createElement("button");
+    clearButton.classList.add("clear-button");
+    clearButton.addEventListener("click", clearList);
+    clearButton.textContent = "clear list";
+    listContainer.appendChild(clearButton);
   }
-}
+};
+
+const clearParent = (item) => {
+  const items = JSON.parse(localStorage.getItem("items")) || [];
+  const updatedItems = items.filter((i) => i !== item);
+  localStorage.setItem("items", JSON.stringify(updatedItems));
+  document.getElementsByClassName("times")[0].parentNode.parentNode.remove();
+  if (!localStorage.getItem("items"))
+    document.getElementsByClassName("clear-button")[0].remove();
+  window.location.reload();
+};
+
+
+const addNeeded = (item) => {
+  if (item) {
+    const items = JSON.parse(localStorage.getItem("items")) || [];
+    items.unshift(item);
+    localStorage.setItem("items", JSON.stringify(items));
+    addItemInput.value = "";
+    initialItems(items);
+    window.location.reload();
+  }
+};
+
+const clearList = () => {
+  localStorage.clear();
+  document.getElementsByClassName("clear-button")[0].parentNode.remove();
+};
+
+// initialize content
+initialItems(shoppingList);
