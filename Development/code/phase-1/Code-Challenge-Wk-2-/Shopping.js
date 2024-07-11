d527f6925f0f98fa2896d4a0ab17e93deae28469
Nyambane F Godfrey <nyambanego@gmail.com>
2024-07-11 21:17:03 +0300

    Add new changes

--- Development/code/phase-1/Code-Challenge-Wk-2-/Shopping.js
+++ Development/code/phase-1/Code-Challenge-Wk-2-/Shopping.js
@@ -1,17 +1,15 @@
-const Shopping List= ["Dairy Milk", "Rice", "Chicken", "Pilau Masala", "Coconut"]
+const shoppingList=["Dairy Milk", "Rice", "Chicken", "Pilau Masala", "Coconut"]
 
+const container=document.getElementById('itemsContainer')
 //iterate through the array and render the array as a list item
 
-Shopping List.map((item))=>{
+shoppingList.map((item)=>{
   // create a list item
   // add the item as a text content
   // add it to the DOM
 
   let myListItem=document.createElement('li')
-  myListItem.innerHTML =item
-
-  let container=document.getElementById('itemsContainer')
-
-  container.appendChild(myListItem)
-
-}
+  myListItem.innerHTML= item
+  container.appendChild('myListItem')
+  
+})
